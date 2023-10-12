4a3e66de1a49a8d376ab24ed183015131d4fd621
Ricardo Tonholli <ricardotonholli@gmail.com>
2023-10-12 04:45:47 -0300

    corrigido bug no doc. JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
-console.log(`Deu bom caralho!!!`)
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi(pokemon) {
+    return`<span class="number">#006</span>
+    <span class="name"> ${pokemon.name} </span>
+
+    <div class="detail">
+        <ol class="types">
+            <li class="type">Grass</li>
+            <li class="type"> Poison</li>
+        </ol>
+        <img src="assets/images/charizard.png" alt="">
+    </div>`
+}
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results) 
+    .then((pokemonList) => {
+        
+        for (let i = 0; i < pokemonList.length; i++) {
+            let pokemon = pokemonList[i] ;
+            console.log(convertPokemonToLi(pokemon))
+            
+        }
+    }
+    )
+    .catch((error) => console.log(error))
+
+    
